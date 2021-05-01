a095d028f59d108d6a94b0edcc0fd83a6fc54cfa
Michael <michael@porkbrain.com>
2021-05-01 17:20:17 +0100

    Iterating local files

--- dealc/src/main.rs
+++ dealc/src/main.rs
@@ -1,5 +1,27 @@
 mod phrase;
 
+use std::{fs, path::PathBuf};
+
+use shared::vision::Annotation;
+
 fn main() {
-    println!("Hello, world!");
+    for file in fs::read_dir("dealc/.local").unwrap() {
+        let file = file.unwrap();
+
+        if file.path().extension().is_some() {
+            continue;
+        }
+
+        let json: Annotation =
+            serde_json::from_str(&fs::read_to_string(file.path()).unwrap())
+                .unwrap();
+
+        let phrases = phrase::from_text(&json.text).join("\n");
+        fs::write(
+            PathBuf::from("clis/phrases")
+                .join(file.path().file_name().unwrap()),
+            phrases,
+        )
+        .unwrap();
+    }
 }
