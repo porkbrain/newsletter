b52959dc5ad79e80100e2f077e52977f8639b7a2
Michael <michael@porkbrain.com>
2021-05-01 19:00:00 +0100

    Training deals classifier

--- voucherc/py-nn/src/train_deals.py
+++ voucherc/py-nn/src/train_deals.py
@@ -11,6 +11,7 @@ from sklearn.datasets import load_files
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 import pickle
+import datetime
 
 
 def load_lines(f):
@@ -32,12 +33,16 @@ def normalize(phrases):
     stemmer = WordNetLemmatizer()
 
     for sen in range(0, len(phrases)):
-        document = re.sub(r"\W", " ", str(phrases[sen]))
-        document = re.sub(r"\s+[a-zA-Z]\s+", " ", document)
-        document = re.sub(r"\^[a-zA-Z]\s+", " ", document)
-        document = re.sub(r"\s+", " ", document, flags=re.I)
+        document = str(phrases[sen]).lower()
+        document = re.sub(r"\$", " USD ", document)
+        document = re.sub(r"£", " GBP ", document)
+        document = re.sub(r"€", " EUR ", document)
+        document = re.sub(r"%", " PERCENT ", document)
+        document = re.sub(r"\W", " ", document)
+        document = re.sub(r"\s+[a-z]\s+", " ", document)
+        document = re.sub(r"\^[a-z]\s+", " ", document)
+        document = re.sub(r"\s+", " ", document)
         document = re.sub(r"^b\s+", "", document)
-        document = document.lower()
 
         documents.append(document)
 
@@ -46,10 +51,10 @@ def normalize(phrases):
 
 vectorizer = CountVectorizer(max_features=1500, min_df=0.005, max_df=0.8)
 tfidfconverter = TfidfTransformer()
-classifier = RandomForestRegressor(n_estimators=1000, random_state=0, n_jobs=16)
+classifier = RandomForestRegressor(n_estimators=1000, n_jobs=16, verbose=1)
 
-deals = load_lines("../data/deals.txt")
-ndeals = load_lines("../data/ndeals.txt")
+deals = load_lines("../data/deals.en.txt")
+ndeals = load_lines("../data/ndeals.en.txt")
 
 X = np.concatenate((deals, ndeals))
 y = np.concatenate(
@@ -63,30 +68,11 @@ np.random.shuffle(y)
 
 X = vectorizer.fit_transform(X).toarray()
 X = tfidfconverter.fit_transform(X).toarray()
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.1, random_state=0
-)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 classifier.fit(X_train, y_train)
 
+print()
 y_pred = classifier.predict(X_test)
 print("Mean Absolute Error:", metrics.mean_absolute_error(y_test, y_pred))
 print("Mean Squared Error:", metrics.mean_squared_error(y_test, y_pred))
-print(
-    "Root Mean Squared Error:",
-    np.sqrt(metrics.mean_squared_error(y_test, y_pred)),
-)
-
-"""
-test_lines = load_lines("../data/ndeals2.txt")
-y_test = np.zeros((1, test_lines.size))[0]
-X_test = vectorizer.transform(test_lines).toarray()
-X_test = tfidfconverter.transform(X_test).toarray()
-
-y_pred = classifier.predict(X_test)
-for x in range(0, len(y_pred)):
-    if y_pred[x] == 1.0:
-        print(x, test_lines[x])
-print(confusion_matrix(y_test, y_pred))
-print(accuracy_score(y_test, y_pred))
-"""
